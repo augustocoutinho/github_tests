0a461f8920f6e5b96cb2dbfdf0b5b7799cb370da
augustocoutinho <augustocef10@gmail.com>
2024-09-03 20:56:29 -0300

    correção dos locais 2

--- app/app.py
+++ app/app.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-path = './input/data_1.csv'
+path = 'input/data_1.csv'
 
 df = pd.read_csv(path)
-df.to_csv('./output/output.csv', index=False)
+df.to_csv('output/output.csv', index=False)
 
 print(df)
